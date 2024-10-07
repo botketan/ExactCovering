55beb05b596a27da095125f00a76a4134ca1d08e
botketan <ketansingh918@gmail.com>
2024-10-07 22:57:40 +0530

    PairWise Encoding

--- inputGeneration.cpp
+++ inputGeneration.cpp
@@ -2,7 +2,7 @@
 #include <bitset>
 using namespace std;
 
-void prettyPrint(vector<set<int>> &matrix){
+void prettyPrintMatrix(vector<set<int>> &matrix){
     freopen ("input_matrix.txt", "w", stdout);
     for(auto row :matrix){
         for(auto item : row){
@@ -24,11 +24,50 @@ vector<set<int>> generateMatrix(int n,int k){
     return nrows;
 }
 
+vector<vector<int>> genClauses(vector<set<int>>& matrix,int k){
+    vector<vector<int>> ItemsMapping(k+1);
+    int n = matrix.size();
+    for(int i =0;i<n;i++){
+        for(auto item:matrix[i]){
+            ItemsMapping[item].push_back(i+1);
+        }
+    }
+    vector<vector<int>> Clauses;
+    for(int i =1;i<=k;i++){
+        vector<int> mainClause;
+        for(auto x:ItemsMapping[i]){
+            mainClause.push_back(x);
+        }
+        mainClause.push_back(0);
+        Clauses.push_back(mainClause);
+        for(int j =0;j<ItemsMapping[i].size();j++){
+            for(int k = j+1;k<ItemsMapping[i].size();k++){
+                Clauses.push_back({-ItemsMapping[i][j],-ItemsMapping[i][k],0});
+            }
+        }
+    }
+    return Clauses;
+}
+
+void prettyPrintClauses(vector<vector<int>>&clauses,int n){
+    freopen ("pairwise_encoding.txt", "w", stdout);
+    cout<<"p cnf "<<n<<" "<<clauses.size()<<endl;
+    for(auto clause : clauses){
+        for(int i =0;i<clause.size()-1;i++){
+            cout<<clause[i]<<" ";
+        }
+        cout<<clause[clause.size()-1]<<endl;
+    }
+}
+
+
 int main(){
     srand(time(0));
     cout<<"Input n and k :";
     int n,k;
     cin>>n>>k;
     auto matrix = generateMatrix(n,k);
-    prettyPrint(matrix);
+    prettyPrintMatrix(matrix);
+    auto clauses = genClauses(matrix,k);
+    prettyPrintClauses(clauses,n);
 }
